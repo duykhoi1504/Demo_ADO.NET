8f35749f926a5e283ae1bf54940f59b928ff42f7
duykhoi1504 <111678054+duykhoi1504@users.noreply.github.com>
2025-04-30 09:40:40 +0700

    save change

--- CoffeeManagement/CoffeeManagement/obj/Debug/net9.0-windows/PresentationLayer.AssemblyInfo.cs
+++ CoffeeManagement/CoffeeManagement/obj/Debug/net9.0-windows/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b0593e22fb99230f2acb5a49387cb12808bf56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a422b4f07a3716e956dc36b1c064f789fde5021")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
